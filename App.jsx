924886e44f11765a93975b8f8e00e2f3d93e2eb6
stvyn-namdy <webmetcreatives@gmail.com>
2025-03-10 00:36:19 -0600

    Lab3: Upedate State and Passed props

--- App.jsx
+++ App.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native'
 import TodoList from './ToDoList'
 import TodoForm from './ToDoForm'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    'Do Laundry',
+    'Go to Gym',
+    'Walk dog'
+  ]);
+
   return (
     <SafeAreaView>
-      <TodoList />
+      <TodoList tasks={tasks} />
       <TodoForm />
     </SafeAreaView>
   )
